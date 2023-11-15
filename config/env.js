eb5e2e1da78d2fe7fc5c0337125e76526e0f3ad2
MaidedPonce <maided.h.p90@gmail.com>
2023-11-14 23:36:34 -0600

    Updates

---
+++ config/env.js
@@ -0,0 +1,9 @@
+const config = {
+  CLIENT_ID: '0dc204362a734dc3b0d0b14e9a8c5f9f',
+  CLIENT_SECRET: '848e7a1734cc49ab86cf19fba1f2fc95',
+  CLIENT: 'https://api.spotify.com/v1',
+  PLAYLIST_ID: '42jFpSBQ6bwzhKdqKVPEoJ',
+  AUTH_CLIENT: 'https://accounts.spotify.com/api/token'
+}
+
+export default config
